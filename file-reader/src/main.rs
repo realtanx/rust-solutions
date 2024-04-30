fd34494cce513f4fc4a58d9ab36e7211caac7ab4
realtanx <hamniba8@gmail.com>
2024-04-30 16:08:38 +0800

    Use struct methods

--- file-reader/src/main.rs
+++ file-reader/src/main.rs
@@ -4,9 +4,32 @@ use std::{
 };
 
 #[derive(Debug)]
-struct FileModel {
+struct FileObj {
     name: String,
     data: Vec<u8>,
+}
+
+impl FileObj {
+    fn new(name: &str) -> FileObj {
+        FileObj {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> FileObj {
+        let mut f = FileObj::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    fn read(self: &FileObj, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let len = tmp.len();
+        save_to.reserve(len);
+        save_to.append(&mut tmp);
+        len
+    }
 }
 
 fn main() {
@@ -72,7 +95,7 @@ fn file_read() {
 // }
 
 fn file_struct() {
-    let f1 = FileModel {
+    let f1 = FileObj {
         name: String::from("file1.txt"),
         data: Vec::new(),
     };
@@ -85,37 +108,33 @@ fn file_struct() {
 
     print_divider();
 
-    let mut f2 = FileModel {
-        name: String::from("file2.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let f2_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f2 = FileObj::new_with_data("file2.txt", &f2_data);
     let mut buffer: Vec<u8> = vec![];
     open(&mut f2);
-    let f2_len = read(&f2, &mut buffer);
+    let f2_len = f2.read(&mut buffer);
     close(&mut f2);
 
     let text = String::from_utf8_lossy(&buffer);
     println!("{:?}", f2);
     println!("{} is {} bytes long", f2.name, f2_len);
     println!("{} text is: {}", f2.name, text);
+
+    print_divider();
+
+    let f3 = FileObj::new("file3.txt");
+    let f3_name = &f3.name;
+    let f3_len = f3.data.len();
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", f3_name, f3_len);
 }
 
-fn open(f: &mut FileModel) -> bool {
+fn open(f: &mut FileObj) -> bool {
     true
 }
 
-fn close(f: &mut FileModel) -> bool {
+fn close(f: &mut FileObj) -> bool {
     true
-}
-
-fn read(f: &FileModel, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
-    let len = tmp.len();
-
-    save_to.reserve(len);
-    save_to.append(&mut tmp);
-
-    len
 }
 
 fn print_divider() {
