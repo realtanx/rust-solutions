cc502f97966cc01e5064227963ee444e874a3a32
realtanx <hamniba8@gmail.com>
2024-04-30 10:56:52 +0800

    Read by lines

--- file-reader/src/main.rs
+++ file-reader/src/main.rs
@@ -7,31 +7,44 @@ fn main() {
     let f = File::open("../README.md");
     match f {
         Ok(file) => {
-            let mut reader = BufReader::new(file);
-            let mut line = String::new();
+            let reader = BufReader::new(file);
 
             // 1: read by line
-            loop {
-                let len = reader.read_line(&mut line);
-                match len {
-                    Ok(size) => {
-                        if size == 0 {
-                            println!("\n EOF.");
-                            break;
-                        }
+            // let mut line = String::new();
+            // loop {
+            //     let len = reader.read_line(&mut line);
+            //     match len {
+            //         Ok(size) => {
+            //             if size == 0 {
+            //                 println!("\n EOF.");
+            //                 break;
+            //             }
 
-                        println!("{} ({} bytes long)", line, size);
+            //             println!("{} ({} bytes long)", line, size);
 
-                        // Shrinks the String back to length 0
-                        // preventing lines from persisting into the following ones.
-                        line.truncate(0);
+            //             // Shrinks the String back to length 0
+            //             // preventing lines from persisting into the following ones.
+            //             line.truncate(0);
+            //         }
+            //         Err(err) => {
+            //             println!("read line error: {}", err);
+            //             break;
+            //         }
+            //     }
+            // }
+
+            // 2: read by lines
+            for line in reader.lines() {
+                match line {
+                    Ok(s) => {
+                        println!("{} ({} bytes long)", s, s.len());
                     }
-                    Err(err) => {
-                        println!("read line error: {}", err);
-                        break;
+                    Err(e) => {
+                        println!("read line error: {}", e);
                     }
                 }
             }
+            println!("\n EOF.");
         }
         Err(err) => {
             println!("open file error: {}", err);
